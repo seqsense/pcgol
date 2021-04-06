seqsense/pcdeditor

Optimize vector access

pcd/iterator.go
@@ -3,6 +3,8 @@ package pcd
 import (
 	"encoding/binary"
 	"math"
+
+	"github.com/seqsense/pcdviewer/mat"
 )
 
 type binaryIterator struct {
@@ -23,6 +25,12 @@ type Float32Iterator interface {
 	Incr()
 	IsValid() bool
 	Float32() float32
+}
+
+type Vec3Iterator interface {
+	Incr()
+	IsValid() bool
+	Vec3() mat.Vec3
 }
 
 type binaryFloat32Iterator struct {
@@ -51,4 +59,23 @@ func (i *float32Iterator) IsValid() bool {
 
 func (i *float32Iterator) Float32() float32 {
 	return i.data[i.pos]
+}
+
+func (i *float32Iterator) Vec3() mat.Vec3 {
+	return mat.Vec3{i.data[i.pos], i.data[i.pos+1], i.data[i.pos+2]}
+}
+
+type naiveVec3Iterator [3]Float32Iterator
+
+func (i naiveVec3Iterator) IsValid() bool {
+	return i[0].IsValid()
+}
+
+func (i naiveVec3Iterator) Incr() {
+	i[0].Incr()
+	i[1].Incr()
+	i[2].Incr()
+}
+func (i naiveVec3Iterator) Vec3() mat.Vec3 {
+	return mat.Vec3{i[0].Float32(), i[1].Float32(), i[2].Float32()}
 }

seqsense/pcgol

pc/iterator.go
@@ -3,6 +3,8 @@ package pc
 import (
 	"encoding/binary"
 	"math"
+
+	"github.com/seqsense/pcgol/mat"
 )
 
 type binaryIterator struct {
@@ -23,6 +25,12 @@ type Float32Iterator interface {
 	Incr()
 	IsValid() bool
 	Float32() float32
+}
+
+type Vec3Iterator interface {
+	Incr()
+	IsValid() bool
+	Vec3() mat.Vec3
 }
 
 type binaryFloat32Iterator struct {
@@ -51,4 +59,23 @@ func (i *float32Iterator) IsValid() bool {
 
 func (i *float32Iterator) Float32() float32 {
 	return i.data[i.pos]
+}
+
+func (i *float32Iterator) Vec3() mat.Vec3 {
+	return mat.Vec3{i.data[i.pos], i.data[i.pos+1], i.data[i.pos+2]}
+}
+
+type naiveVec3Iterator [3]Float32Iterator
+
+func (i naiveVec3Iterator) IsValid() bool {
+	return i[0].IsValid()
+}
+
+func (i naiveVec3Iterator) Incr() {
+	i[0].Incr()
+	i[1].Incr()
+	i[2].Incr()
+}
+func (i naiveVec3Iterator) Vec3() mat.Vec3 {
+	return mat.Vec3{i[0].Float32(), i[1].Float32(), i[2].Float32()}
 }