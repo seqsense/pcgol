59fc81c4cef66881cc355608f62ceb24d3f9620d
Atsushi Watanabe <atsushi.w@ieee.org>
2021-10-07 22:55:24 +0900

    Add Mat4.Transpose

--- mat/mat4_test.go
+++ mat/mat4_test.go
@@ -140,3 +140,22 @@ func TestMat4_Inv(t *testing.T) {
 		}
 	}
 }
+
+func TestMat4_Transpose(t *testing.T) {
+	m := Mat4{
+		1, 2, 3, 4,
+		5, 6, 7, 8,
+		9, 10, 11, 12,
+		13, 14, 15, 16,
+	}
+	expected := Mat4{
+		1, 5, 9, 13,
+		2, 6, 10, 14,
+		3, 7, 11, 15,
+		4, 8, 12, 16,
+	}
+	trans := m.Transpose()
+	if !reflect.DeepEqual(expected, trans) {
+		t.Errorf("Expected %v, got %v", expected, trans)
+	}
+}
