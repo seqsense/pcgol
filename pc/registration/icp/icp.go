4b2209f905538f2ad46a53f8c548c336c60c9fa0
Atsushi Watanabe <atsushi.w@ieee.org>
2021-10-09 19:12:23 +0900

    Make update weight configurable

--- pc/registration/icp/icp.go
+++ pc/registration/icp/icp.go
@@ -15,9 +15,12 @@ var (
 	ErrNeedGradient = errors.New("need gradient output of Evaluator")
 )
 
+var DefaultGradientWeight = mat.Vec6{0.3, 0.3, 0.3, 0.3, 0.3, 0.3}
+
 type PointToPointICPGradient struct {
-	Evaluator    Evaluator
-	MaxIteration int
+	Evaluator      Evaluator
+	MaxIteration   int
+	GradientWeight mat.Vec6
 }
 
 func (r *PointToPointICPGradient) Fit(base storage.Search, target pc.Vec3RandomAccessor) (mat.Mat4, error) {
@@ -41,22 +44,28 @@ func (r *PointToPointICPGradient) Fit(base storage.Search, target pc.Vec3RandomA
 	if maxIteration == 0 {
 		maxIteration = 20
 	}
+	weight := r.GradientWeight
+	if weight.Equal(mat.Vec6{}) {
+		weight = DefaultGradientWeight
+	}
+
 	trans := mat.Translate(0, 0, 0)
 	for i := 0; i < maxIteration; i++ {
 		ev, err := r.Evaluator.Evaluate(base, targetTransformed)
 		if err != nil {
 			return trans, err
 		}
-		factor := -0.3 * (1 - (float32(i) / float32(maxIteration)))
+
+		factorIter := -(1 - (float32(i) / float32(maxIteration)))
 		deltaTrans := mat.Translate(
-			factor*ev.Gradient[dX],
-			factor*ev.Gradient[dY],
-			factor*ev.Gradient[dZ],
+			factorIter*weight[0]*ev.Gradient[dX],
+			factorIter*weight[1]*ev.Gradient[dY],
+			factorIter*weight[2]*ev.Gradient[dZ],
 		)
 		deltaRot := rodriguesToRotation(mat.Vec3{
-			factor * ev.Gradient[dWx],
-			factor * ev.Gradient[dWy],
-			factor * ev.Gradient[dWz],
+			factorIter * weight[3] * ev.Gradient[dWx],
+			factorIter * weight[4] * ev.Gradient[dWy],
+			factorIter * weight[5] * ev.Gradient[dWz],
 		})
 		trans = deltaRot.Mul(deltaTrans.Mul(trans))
 		for i := 0; i < target.Len(); i++ {
