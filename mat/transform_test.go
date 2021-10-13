ab9c24e09837079d426c1d5ca0af789980c20849
Atsushi Watanabe <atsushi.w@ieee.org>
2021-10-13 17:39:18 +0900

    Add test cases

--- mat/transform_test.go
+++ mat/transform_test.go
@@ -45,6 +45,16 @@ func TestRotate(t *testing.T) {
 			v:        Vec3{2, 0, 0},
 			expected: Vec3{math.Sqrt2, math.Sqrt2, 0},
 		},
+		"yaw=+45deg,l=1,Crossing": {
+			m:        Rotate(0, 0, 1, math.Pi/4),
+			v:        Vec3{0, 0, 1},
+			expected: Vec3{0, 0, 1},
+		},
+		"yaw=45+45deg,l=1": {
+			m:        Rotate(0, 0, 1, math.Pi/4),
+			v:        Vec3{math.Sqrt2 / 2, math.Sqrt2 / 2, 0},
+			expected: Vec3{0, 1, 0},
+		},
 		"yaw=+45deg,l=1": {
 			m:        Rotate(0, 0, 1, math.Pi/4),
 			v:        Vec3{1, 0, 0},
@@ -118,12 +128,32 @@ func TestRotate(t *testing.T) {
 			expected: Vec3{-math.Sqrt2 / 2, 0, -math.Sqrt2 / 2},
 		},
 	}
+
+	const tolerance = 1e-6
+	nearlyEqual := func(a, b float32) bool {
+		return -tolerance < a-b && a-b < tolerance
+	}
+	if !nearlyEqual(1.0, 1.0000009) {
+		t.Fatal("internal: tolerance check")
+	}
+	if nearlyEqual(1.0, 1.0000011) {
+		t.Fatal("internal: tolerance check")
+	}
+	if !nearlyEqual(1.0, 0.9999991) {
+		t.Fatal("internal: tolerance check")
+	}
+	if nearlyEqual(1.0, 0.999998) {
+		t.Fatal("internal: tolerance check")
+	}
+
 	for name, tt := range testCases {
 		tt := tt
 		t.Run(name, func(t *testing.T) {
 			out := tt.m.Transform(tt.v)
 
-			if !out.Equal(tt.expected) {
+			if !nearlyEqual(out[0], tt.expected[0]) ||
+				!nearlyEqual(out[1], tt.expected[1]) ||
+				!nearlyEqual(out[2], tt.expected[2]) {
 				t.Errorf("%s transformed by %s is expected to be %s, got %s",
 					tt.v, tt.m, tt.expected, out,
 				)
