cd29d3fd009e6d8172899d8fed03c2334083d5d0
Atsushi Watanabe <atsushi.w@ieee.org>
2025-07-16 17:04:34 +0900

    pc/filter/voxelgrid: add chunk option (#60)
    Add option to chunk the voxel grid to allow filtering spatially large points on low memory

--- pc/filter/voxelgrid/voxelgrid.go
+++ pc/filter/voxelgrid/voxelgrid.go
@@ -6,12 +6,12 @@ import (
 	"github.com/seqsense/pcgol/pc/filter"
 )
 
-type Options struct {
-	LeafSize mat.Vec3
-}
+const initialSliceCap = 256
 
 type voxelGrid struct {
 	Options
+
+	voxels []voxel
 }
 
 type voxel struct {
@@ -20,11 +20,14 @@ type voxel struct {
 	index int
 }
 
-func New(leafSize mat.Vec3) filter.Filter {
+func New(leafSize mat.Vec3, opts ...Option) filter.Filter {
 	vg := &voxelGrid{
 		Options: Options{
 			LeafSize: leafSize,
 		},
+	}
+	for _, o := range opts {
+		o(&vg.Options)
 	}
 	return vg
 }
@@ -34,27 +37,117 @@ func (f *voxelGrid) Filter(pp *pc.PointCloud) (*pc.PointCloud, error) {
 	if err != nil {
 		return nil, err
 	}
-	min, max, err := pc.MinMaxVec3(it)
+
+	vMin, vMax, err := pc.MinMaxVec3(it)
 	if err != nil {
 		return nil, err
 	}
+	if f.ChunkSize[0]*f.ChunkSize[1]*f.ChunkSize[2] == 0 {
+		return f.filterChunk(vMin, vMax, it, pp)
+	}
 
-	size := max.Sub(min)
+	size := vMax.Sub(vMin)
+	chunkSize := mat.Vec3{
+		f.LeafSize[0] * float32(f.ChunkSize[0]),
+		f.LeafSize[1] * float32(f.ChunkSize[1]),
+		f.LeafSize[2] * float32(f.ChunkSize[2]),
+	}
+	nx, ny, nz := int(size[0]/chunkSize[0])+1, int(size[1]/chunkSize[1])+1, int(size[2]/chunkSize[2])+1
+	nChunks := nx * ny * nz
+
+	outs := make([]*pc.PointCloud, 0, nChunks)
+	indices := make([][]int, nChunks)
+	nIndices := make([]int, nChunks)
+
+	cid2xyz := func(cid int) mat.Vec3 {
+		x := cid % nx
+		cid = cid / nx
+		y := cid % ny
+		z := cid / ny
+		return mat.Vec3{float32(x), float32(y), float32(z)}
+	}
+	vec2cid := func(p mat.Vec3) int {
+		x, y, z := int(p[0]/chunkSize[0]), int(p[1]/chunkSize[1]), int(p[2]/chunkSize[2])
+		return ((z*ny)+y)*nx + x
+	}
+
+	defer func() {
+		// Make large slice GC-ed ASAP
+		f.voxels = nil
+	}()
+
+	// Count points in each chunk and allocate indices
+	for i := 0; i < it.Len(); i++ {
+		cid := vec2cid(it.Vec3At(i).Sub(vMin))
+		nIndices[cid]++
+	}
+	for i := range indices {
+		indices[i] = make([]int, 0, nIndices[i])
+	}
+
+	// Build indice for each chunk
+	for i := 0; i < it.Len(); i++ {
+		cid := vec2cid(it.Vec3At(i).Sub(vMin))
+		indices[cid] = append(indices[cid], i)
+	}
+
+	// Apply filter to the chunks
+	for cid, indice := range indices {
+		if len(indice) == 0 {
+			continue
+		}
+		iit := pc.NewVec3RandomAccessorIterator(
+			pc.NewIndiceVec3RandomAccessor(it, indice),
+		)
+		cp := cid2xyz(cid)
+		vcMin := vMin.Add(cp.ElementMul(chunkSize))
+		out, err := f.filterChunk(vcMin, chunkSize, iit, pp)
+		if err != nil {
+			return nil, err
+		}
+		outs = append(outs, out)
+	}
+
+	// Combine the outputs
+	var n int
+	for _, out := range outs {
+		n += out.Width * out.Height
+	}
+	newPc := &pc.PointCloud{
+		PointCloudHeader: pp.Clone(),
+		Points:           n,
+		Data:             make([]byte, 0, pp.Stride()*n),
+	}
+	newPc.Width = n
+	newPc.Height = 1
+	for _, out := range outs {
+		newPc.Data = append(newPc.Data, out.Data...)
+	}
+	return newPc, nil
+}
+
+func (f *voxelGrid) filterChunk(vMin, size mat.Vec3, it pc.Vec3ConstForwardIterator, pp *pc.PointCloud) (*pc.PointCloud, error) {
 	xs, ys, zs := int(size[0]/f.LeafSize[0]), int(size[1]/f.LeafSize[1]), int(size[2]/f.LeafSize[2])
-	voxels := make([]voxel, (xs+1)*(ys+1)*(zs+1))
+	nVoxels := (xs + 1) * (ys + 1) * (zs + 1)
+	if len(f.voxels) < nVoxels {
+		f.voxels = make([]voxel, nVoxels)
+	} else {
+		for i := range f.voxels {
+			f.voxels[i] = voxel{}
+		}
+	}
 
 	var n int
-	for i := 0; it.IsValid(); it.Incr() {
-		p := it.Vec3().Sub(min)
+	for ; it.IsValid(); it.Incr() {
+		p := it.Vec3().Sub(vMin)
 		x, y, z := int(p[0]/f.LeafSize[0]), int(p[1]/f.LeafSize[1]), int(p[2]/f.LeafSize[2])
-		v := &voxels[x+xs*(y+ys*z)]
+		v := &f.voxels[x+xs*(y+ys*z)]
 		if v.num == 0 {
-			v.index = i
+			v.index = it.RawIndex()
 			n++
 		}
 		v.num++
 		v.sum = v.sum.Add(p)
-		i++
 	}
 
 	newPc := &pc.PointCloud{
@@ -70,13 +163,13 @@ func (f *voxelGrid) Filter(pp *pc.PointCloud) (*pc.PointCloud, error) {
 	}
 	var jStart int
 	stride := pp.Stride()
-	for i := range voxels {
-		v := &voxels[i]
+	for i := range f.voxels {
+		v := &f.voxels[i]
 		if n := v.num; n > 0 {
 			iStart := v.index * stride
 			copy(newPc.Data[jStart:jStart+stride], pp.Data[iStart:iStart+stride])
 			if n > 1 {
-				jt.SetVec3(v.sum.Mul(1.0 / float32(n)).Add(min))
+				jt.SetVec3(v.sum.Mul(1.0 / float32(n)).Add(vMin))
 			}
 			jt.Incr()
 			jStart += stride
--- pc/filter/voxelgrid/voxelgrid_test.go
+++ pc/filter/voxelgrid/voxelgrid_test.go
@@ -10,61 +10,101 @@ import (
 )
 
 func TestVoxelGrid(t *testing.T) {
-	pp := pc.PointCloud{
-		PointCloudHeader: pc.PointCloudHeader{
-			Fields: []string{"x", "y", "z", "label"},
-			Size:   []int{4, 4, 4, 4},
-			Count:  []int{1, 1, 1, 1},
-			Width:  5,
-			Height: 1,
+	testCases := map[string]struct {
+		opts           []Option
+		expected       []mat.Vec3
+		expectedLabels []uint32
+	}{
+		"Default": {
+			expected: []mat.Vec3{
+				{0.0000, 3.0000, 0.0000},
+				{0.6375, 1.8750, 0.1375},
+				{1.2500, 0.0000, 1.2500},
+				{1.2500, 1.2625, 1.2500},
+			},
+			expectedLabels: []uint32{
+				6, 1, 4, 2,
+			},
 		},
-		Points: 5,
-		Data: float.Float32SliceAsByteSlice([]float32{
-			0.50, 1.50, 0.10, math.Float32frombits(1),
-			1.00, 1.00, 1.00, math.Float32frombits(2),
-			0.52, 1.50, 0.12, math.Float32frombits(3),
-			1.00, 0.00, 1.00, math.Float32frombits(4),
-			1.00, 1.02, 1.00, math.Float32frombits(5),
-		}),
+		"WithChunkSize881": {
+			opts: []Option{WithChunkSize([3]int{8, 8, 1})},
+			expected: []mat.Vec3{
+				{0.0000, 3.0000, 0.0000},
+				{0.6375, 1.8750, 0.1375},
+				{1.2500, 0.0000, 1.2500},
+				{1.2500, 1.2625, 1.2500},
+			},
+			expectedLabels: []uint32{
+				6, 1, 4, 2,
+			},
+		},
+		"WithChunkSize333": {
+			// Output order may vary
+			opts: []Option{WithChunkSize([3]int{3, 3, 3})},
+			expected: []mat.Vec3{
+				{0.6375, 1.8750, 0.1375},
+				{0.0000, 3.0000, 0.0000},
+				{1.2500, 0.0000, 1.2500},
+				{1.2500, 1.2625, 1.2500},
+			},
+			expectedLabels: []uint32{
+				1, 6, 4, 2,
+			},
+		},
 	}
 
-	vg := New(mat.Vec3{0.1, 0.1, 0.1})
-	out, err := vg.Filter(&pp)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for name, tt := range testCases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			pp := pc.PointCloud{
+				PointCloudHeader: pc.PointCloudHeader{
+					Fields: []string{"x", "y", "z", "label"},
+					Size:   []int{4, 4, 4, 4},
+					Count:  []int{1, 1, 1, 1},
+					Width:  6,
+					Height: 1,
+				},
+				Points: 6,
+				Data: float.Float32SliceAsByteSlice([]float32{
+					0.625, 1.875, 0.125, math.Float32frombits(1),
+					1.250, 1.250, 1.250, math.Float32frombits(2),
+					0.650, 1.875, 0.150, math.Float32frombits(3),
+					1.250, 0.000, 1.250, math.Float32frombits(4),
+					1.250, 1.275, 1.250, math.Float32frombits(5),
+					0.000, 3.000, 0.000, math.Float32frombits(6),
+				}),
+			}
 
-	expected := []mat.Vec3{
-		{0.51, 1.50, 0.11},
-		{1.00, 0.00, 1.00},
-		{1.00, 1.01, 1.00},
-	}
-	expectedLabels := []uint32{
-		1, 4, 2,
-	}
+			vg := New(mat.Vec3{0.125, 0.125, 0.125}, tt.opts...)
+			out, err := vg.Filter(&pp)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if len(expected) != out.Points {
-		t.Fatalf("Wrong number of points, expected: %d, got: %d", len(expected), out.Points)
-	}
-	it, err := out.Vec3Iterator()
-	if err != nil {
-		t.Fatal(err)
-	}
-	lt, err := out.Uint32Iterator("label")
-	if err != nil {
-		t.Fatal(err)
-	}
+			if len(tt.expected) != out.Points {
+				t.Fatalf("Wrong number of points, expected: %d, got: %d", len(tt.expected), out.Points)
+			}
+			it, err := out.Vec3Iterator()
+			if err != nil {
+				t.Fatal(err)
+			}
+			lt, err := out.Uint32Iterator("label")
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	for i, e := range expected {
-		p := it.Vec3()
-		if !p.Equal(e) {
-			t.Errorf("Expected point: %v, got: %v", e, p)
-		}
-		l := lt.Uint32()
-		if l != expectedLabels[i] {
-			t.Errorf("Expected label: %x, got: %x", expectedLabels[i], l)
-		}
-		it.Incr()
-		lt.Incr()
+			for i, e := range tt.expected {
+				p := it.Vec3()
+				if !p.Equal(e) {
+					t.Errorf("Expected point: %v, got: %v", e, p)
+				}
+				l := lt.Uint32()
+				if l != tt.expectedLabels[i] {
+					t.Errorf("Expected label: %x, got: %x", tt.expectedLabels[i], l)
+				}
+				it.Incr()
+				lt.Incr()
+			}
+		})
 	}
 }
